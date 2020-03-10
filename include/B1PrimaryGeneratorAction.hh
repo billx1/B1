8a468c5f83e3f4c26eaccaad6420ffcce8c89bfd
John Allison <John.Allison@g4ai.org>
2020-03-10 16:01:23 +0000

    Change header B1PrimaryGeneratorAction.hh for G4GeneralParticleSource

--- include/B1PrimaryGeneratorAction.hh
+++ include/B1PrimaryGeneratorAction.hh
@@ -31,10 +31,10 @@
 #define B1PrimaryGeneratorAction_h 1
 
 #include "G4VUserPrimaryGeneratorAction.hh"
-#include "G4ParticleGun.hh"
+#include "G4GeneralParticleSource.hh"
 #include "globals.hh"
 
-class G4ParticleGun;
+class G4GeneralParticleSource;
 class G4Event;
 class G4Box;
 
@@ -53,10 +53,10 @@ class B1PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
     virtual void GeneratePrimaries(G4Event*);         
   
     // method to access particle gun
-    const G4ParticleGun* GetParticleGun() const { return fParticleGun; }
+    const G4GeneralParticleSource* GetParticleGun() const { return fParticleGun; }
   
   private:
-    G4ParticleGun*  fParticleGun; // pointer a to G4 gun class
+    G4GeneralParticleSource*  fParticleGun; // pointer a to G4 gun class
     G4Box* fEnvelopeBox;
 };
 
